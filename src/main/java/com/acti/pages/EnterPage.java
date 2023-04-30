ac1a24e30c87477e1deebf98fc9b7c1fa58ee4cf
Other <Other@169.254.225.111>
2023-04-29 23:42:09 -0700

    Added constructor to EnterPage

--- src/main/java/com/acti/pages/EnterPage.java
+++ src/main/java/com/acti/pages/EnterPage.java
@@ -1,16 +1,29 @@
 package com.acti.pages;
 
+import org.openqa.selenium.support.PageFactory;
+
 import com.acti.base.DriverScript;
+
+/* 
+ * Name: EnterPage
+ * Description: Manages the locators and methods of EnterPage
+ * Developed By: Sathya
+ * Reviewed By: Shantosh
+ * Date Reviewed: 04-29-2023
+ */
 
 public class EnterPage extends DriverScript {
 	
-	/* 
-	 * Name: EnterPage
-	 * Description: Manages the locators and methods of EnterPage
-	 * Developed By: Sathya
-	 * Reviewed By: Shantosh
-	 * Date Reviewed: 04-29-2023
-	 */
+// *********************************** Page Locators **********************************//
+		
 	
-
+// *********************************** Page Initialization ****************************//	
+	
+	public EnterPage() 
+	{
+		PageFactory.initElements (driver, this);
+	}
+		
+// *********************************** Page Methods *********************************//	
+	
 }
