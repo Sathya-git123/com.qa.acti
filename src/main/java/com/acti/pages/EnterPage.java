e4631d67a2e20d8ca34f9b5a1cae8c33c31a5893
Other <Other@169.254.225.111>
2023-04-29 23:30:52 -0700

    Author details added to EnterPage

--- src/main/java/com/acti/pages/EnterPage.java
+++ src/main/java/com/acti/pages/EnterPage.java
@@ -4,5 +4,13 @@ import com.acti.base.DriverScript;
 
 public class EnterPage extends DriverScript {
 	
+	/* 
+	 * Name: EnterPage
+	 * Description: Manages the locators and methods of EnterPage
+	 * Developed By: Sathya
+	 * Reviewed By: Shantosh
+	 * Date Reviewed: 04-29-2023
+	 */
+	
 
 }
